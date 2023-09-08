62089bda63c40b6f6a3eec8aab748a17bec11e3b
Thiago-Cruz-eng <th24093@gmail.com.br>
2023-09-08 15:24:47 -0300

    feat: implement automapper

---
+++ LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
