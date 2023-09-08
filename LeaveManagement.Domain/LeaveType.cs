ef95f1699f6ede575a86a1f44b0e71d28fe0f025
Thiago-Cruz-eng <th24093@gmail.com.br>
2023-09-08 11:01:09 -0300

    feat: start domain class

---
+++ LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
